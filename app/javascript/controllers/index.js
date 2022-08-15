f7f9e951f731eebba57475b903ea211124599abf
BL4CKM4G3 <alvaro_rangel@outlook.com>
2022-08-15 15:36:23 -0300

    feat(:sparkles:): added first table stimulus controller implementation

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,7 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
+import TableController from './table_controller'
+
 application.register("hello", HelloController)
+application.register("table", TableController)
---
+++ app/javascript/controllers/table_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from '@hotwired/stimulus'
+import { helper, notifier, rows } from '../utils'
+import services from '../services'
+
+export default class TableController extends Controller {
+  static targets = ['number-input', 'button', 'no-data-info']
+
+  async search() {
+    let input = this.targets.find('number-input')
+    let button = this.targets.find('button')
+    button.disabled = true
+    button.classList.add('cursor-not-allowed')
+    button.innerText = "Loading..."
+
+    if (helper.checkRecord(input.value)) {
+      rows.move(input.value)
+      notifier.notify('Found provider. Moving row to top...', 'info')
+    }
+
+    if (!helper.checkRecordExists(input.value)) {
+      let provider = await services.providers.fetchNpi(input.value)
+
+      if (provider) {
+        rows.add(provider)
+      }
+
+      let noDataInfo = this.targets.find('no-data-info')
+      if (noDataInfo) {
+        noDataInfo.parentElement.removeChild(noDataInfo)
+      }
+    }
+
+    button.disabled = false
+    button.classList.remove('cursor-not-allowed')
+    button.innerText = 'Search'
+  }
+}
