4ea4b45281b658ffe622c170043e6378c1b83f2b
BL4CKM4G3 <alvaro_rangel@outlook.com>
2022-08-15 15:28:17 -0300

    feat(:sparkles:): added routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'providers/index'
+  get 'providers/search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "providers#search"
+  root "providers#index"
 end
